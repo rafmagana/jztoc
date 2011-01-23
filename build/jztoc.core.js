ce80a0162cf7561cb2ce922a069769d5523c3fd3
acatl pacheco <acatl.pacheco@gmail.com>
2011-01-22 20:28:40 -0600

    fixed plugin

--- build/jztoc.core.js
+++ build/jztoc.core.js
@@ -358,6 +358,9 @@ $.ui.uicomponent.subclass("ui.itemrenderer", {
 })(jQuery);
 (function($){
   $.fn.getChild = function(selector){
+	  if (jQuery.type(selector)=="number") {
+		  return $(this).children().eq(selector);
+	  }
     return $(selector, this);
   };
 })(jQuery);
--- src/plugins/getchild.js
+++ src/plugins/getchild.js
@@ -1,5 +1,8 @@
 (function($){
   $.fn.getChild = function(selector){
+	  if (jQuery.type(selector)=="number") {
+		  return $(this).children().eq(selector);
+	  }
     return $(selector, this);
   };
 })(jQuery);
