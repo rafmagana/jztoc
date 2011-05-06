e4028db9ca69e164e18b39e2dfd1e8731516cf6b
Rafael Magana <raf.magana@gmail.com>
2011-05-06 01:29:51 -0500

    tests dry and cleaning up

--- test/quinit.uicomponent.js
+++ test/quinit.uicomponent.js
@@ -1,57 +1,113 @@
 $(function(){
-
 	
-	
-    module("uicomponent");
+    /* UICOMPONENT WIDGET */
+    module("uicomponent widget", {
+		setup : function(){
+			this.uicomponent = $("#testContainer").uicomponenttest();
+		},
+		teardown : function(){
+		}
+	});
     
-    test("widget creation", function(){
-    	
+	test("should be attached correctly", function(){
+		equals(this.uicomponent.uicomponenttest != null, true, "attachment of widget");
+	});
 
-		console.log(jQuery.jztocSettings);
+	test("should execute _created", function(){
+		equals(this.uicomponent.uicomponenttest("option","created"),true, "execution of _create");
+	});
 
-        var uicomponent = $("#testContainer").uicomponenttest();
+	test("should execute _init", function(){
+		equals(this.uicomponent.uicomponenttest("option","initialized"),true, "execution of  _init");
+	});
+
+    /* UICOMPONENT HELPERS */
+    module("uicomponent helpers", {
+		setup : function(){
+			this.uicomponent = $("#testContainer").uicomponenttest();
+		}
+	});
+
+	test("should be extended correctly", function(){
+		equals(this.uicomponent.uicomponenttest("testHelper") ,true, "testing helper");
+	});
+
+	test("should be inherited correctly", function(){
+		equals(this.uicomponent.uicomponenttest("testScope") ,true, "testing helper scope");
+	});
+
+    /* UICOMPONENT STATES */
+    module("uicomponent state", {
+		setup : function(){
+			this.uicomponent = $("#testContainer").uicomponenttest();
+		}
+	});
+
+	test("should be changed to the 'test' state", function(){
+		this.uicomponent.uicomponenttest("currentState", "test");
+		equals(this.uicomponent.attr("data-state"),"test", "testing states");
+	});
+
+    /* UICOMPONENT PROPERTIES */
+    module("uicomponent properties", {
+		setup : function(){
+			this.uicomponent = $("#testContainer").uicomponenttest();
+
+			this.propertyOne = "propertyOne";
+			this.propertyTwo = "propertyTwo";
+			this.propertyOneValue = 1;
+			this.propertyTwoValue = 2;
+			this.propertiesValueToBeChangedTo = 5;
+
+			this.uicomponent.uicomponenttest(this.propertyOne, this.propertyOneValue);
+			this.uicomponent.uicomponenttest(this.propertyTwo, this.propertyTwoValue);
+		}
+	});
+
+	test("propertyOne and propertyTwo should exist", function(){
+		equals(this.uicomponent.uicomponenttest("hasProperty", this.propertyOne), true, "property propertyOne exists");	
+	});
+
+	test("propertyOne and propertyTwo should have the default value assigned", function(){
+		expect(2);
+		equals(this.uicomponent.uicomponenttest(this.propertyOne), this.propertyOneValue, "property propertyOne has correct value");
+		equals(this.uicomponent.uicomponenttest(this.propertyTwo), this.propertyTwoValue, "property propertyOne has correct value");
+	});
+
+	test("value should be able to be changed", function(){
+		var propertyOneOldValue = this.uicomponent.uicomponenttest(this.propertyOne);
+		var propertyTwoOldValue = this.uicomponent.uicomponenttest(this.propertyTwo);
 		
-		// test if widget was attached		
-        equals(uicomponent.uicomponenttest != null, true, "attachment of widget");
-		// test if _created was executed
-		equals(uicomponent.uicomponenttest("option","created"),true, "execution of _create");
-		// test if _init was executed
-		equals(uicomponent.uicomponenttest("option","initialized"),true, "execution of  _init");
+		this.uicomponent.uicomponenttest(this.propertyOne, this.propertiesValueToBeChangedTo);
+		this.uicomponent.uicomponenttest(this.propertyTwo, this.propertiesValueToBeChangedTo);
 		
-		/* helpers */
-		// test if testHelper was extended correctly
-		equals(uicomponent.uicomponenttest("testHelper") ,true, "testing helper");
-		// test if the scope of the helper was inhereted correctly
-		equals(uicomponent.uicomponenttest("testScope") ,true, "testing helper scope");
+		expect(4);
 		
-		/* states */
-		// change currentState to 'test'
-		uicomponent.uicomponenttest("currentState", "test");
-		// test if state was actually changed 
-		equals(uicomponent.attr("data-state"),"test", "testing states");
+		// propertyOne assertions
+		notEqual(propertyOneOldValue, this.uicomponent.uicomponenttest(this.propertyOne));
+		equals(this.uicomponent.uicomponenttest(this.propertyOne), this.propertiesValueToBeChangedTo, "value of propertyOne was changed to " + this.propertiesValueToBeChangedTo);
 		
-		/* properties */
+		// propertyTwo assertions
+		notEqual(propertyTwoOldValue, this.uicomponent.uicomponenttest(this.propertyTwo));
+		equals(this.uicomponent.uicomponenttest(this.propertyTwo), this.propertiesValueToBeChangedTo, "value of propertyTwo was changed to " + this.propertiesValueToBeChangedTo);
 		
-		// test if property was created
-		equals(uicomponent.uicomponenttest("hasProperty", "propertyOne"),true, "property was created");
-		// test if property has the default value assigned
-		equals(uicomponent.uicomponenttest("propertyOne"),1, "property has correct value");
-		// change property value
-		uicomponent.uicomponenttest("propertyOne", 5);
-		// test if property value changed correctly
-		equals(uicomponent.uicomponenttest("propertyOne"),5, "property changed value");
-		// test if second property has kept consistent 
-		equals(uicomponent.uicomponenttest("propertyTwo"),2, "property keept value");
-		// test if scope of property is correct
-		equals(uicomponent.uicomponenttest("testLocalProperty"),true, "checking scope of property");
-		// test if commitProperties was executed
-		equals(uicomponent.attr("data-propertyone"),"200", "commitProperties was executed");
-		
-		/* get children */
-		// test if childSpan was added to the elements hash
-		equals(uicomponent.uicomponenttest("getElements").childSpan != null,true, "testing getChildren");
-		
-    });
-    
-    
+	});
+
+	test("scope should be correct", function(){
+		equals(this.uicomponent.uicomponenttest("testLocalProperty"), true, "checking scope of property");
+	});
+
+	test("should execute commitProperties when property changes", function(){
+		equals(this.uicomponent.attr("data-propertyone"), this.propertyOneValue.toString(), "commitProperties was executed");
+	});
+
+    module("uicomponent children", {
+		setup : function(){
+			this.uicomponent = $("#testContainer").uicomponenttest();
+		}
+	});
+
+	test("childSpan should be added to the elements hash", function(){
+		equals(this.uicomponent.uicomponenttest("getElements").childSpan != null, true, "testing getChildren");
+	});
 });
